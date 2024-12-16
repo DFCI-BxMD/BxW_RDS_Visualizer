d74c965c3f5ced0494d6efb7d822eb98be411b6d
Tarishi Pathak <tarishipathak@Tarishis-MacBook-Pro.local>
2024-12-16 10:45:43 -0500

    Changed DFCI logo in app

--- Rizky Version/Webapp_source/ui.R
+++ Rizky Version/Webapp_source/ui.R
@@ -184,7 +184,7 @@ dashboardPage(
           br(),
         ),
         tags$img(
-          src ="https://raw.githubusercontent.com/DFCI-BxMD/BxW_RDS_Visualizer/blob/main/Rizky%20Version/docs/Home%20_%20Informatics%20and%20Analytics.png",
+          src ="https://github.com/DFCI-BxMD/BxW_RDS_Visualizer/main/Rizky%20Version/docs/Home%20_%20Informatics%20and%20Analytics.png?raw=true",
           style="display: block; margin-left: auto; margin-right: auto; width:35%; height:15%",
         )
         ),
