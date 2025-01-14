2b50ffd342d8ea69d2a74689c765430e1abda58e
Tarishi Pathak <tarishipathak@Tarishis-MBP.lan>
2025-01-13 21:03:09 -0500

    fix violin plot

--- Rizky Version/Webapp_source/operator.R
+++ Rizky Version/Webapp_source/operator.R
@@ -177,7 +177,8 @@ plotVlnplot=eventReactive(input$plotVlnPlot_Button, {
     features = input$VlnPlot_GeneInput,
     group.by = input$VlnPlot_group_by,
     ncol = number_col,
-    same.y.lims = TRUE
+    same.y.lims = TRUE,
+    raster = TRUE
   )
 }
 
