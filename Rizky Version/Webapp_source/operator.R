fb112457348d8072d2792493972ab2dc26e67ea0
Tarishi Pathak <tarishipathak@dhcp-10-24-77-231.dfci.harvard.edu>
2025-01-29 11:01:54 -0500

    fix scroll

--- Rizky Version/Webapp_source/operator.R
+++ Rizky Version/Webapp_source/operator.R
@@ -147,7 +147,7 @@ plotFeaturePlot=eventReactive(input$plotFeaturePlot_Button, {
   if (reactivevalue$Loaded) {
     waitress$start()
     
-    plot=FeaturePlot(reactivevalue$SeuratObject,features = input$FeaturePlot_GeneInput,reduction = input$FeaturePlot_reduction,order = T, png.arguments = c(50, 50, 500))
+    plot=FeaturePlot(reactivevalue$SeuratObject,features = input$FeaturePlot_GeneInput,reduction = input$FeaturePlot_reduction,order = T)
     
     output$FeaturePlot=renderPlot(plot)
 
